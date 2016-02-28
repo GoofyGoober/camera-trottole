1b22418ab8a2caafd2b08a30acf1bfed2859c7f6
Alessandro <descovi@gmail.com>
2016-02-28 19:10:16 +0100

    Camera can see

--- camera/ViewController.swift
+++ camera/ViewController.swift
@@ -7,12 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    let captureSession = AVCaptureSession()
+    
+    // If we find a device we'll store it here for later use
+    var captureDevice : AVCaptureDevice?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+       findDevice()
+        
+            beginSession()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func findDevice(){
+        captureSession.sessionPreset = AVCaptureSessionPreset1920x1080
+        
+        let devices = AVCaptureDevice.devices()
+        
+        // Loop through all the capture devices on this phone
+        for device in devices {
+            // Make sure this particular device supports video
+            if (device.hasMediaType(AVMediaTypeVideo)) {
+                // Finally check the position and confirm we've got the back camera
+                if(device.position == AVCaptureDevicePosition.Back) {
+                    captureDevice = device as? AVCaptureDevice
+                }
+            }
+        }
+    }
+    
+    func beginSession() {
+        
+        let input : AVCaptureDeviceInput?
+        
+        input = try! AVCaptureDeviceInput(device: captureDevice)
+        captureSession.addInput(input);
+
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.view.layer.addSublayer(previewLayer)
+        previewLayer.frame = self.view.layer.frame
+        captureSession.startRunning()
     }
 
     override func didReceiveMemoryWarning() {
