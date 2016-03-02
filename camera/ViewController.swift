12da53eeb0e087a864894c992490c53cf85f9f1a
Alessandro <descovi@gmail.com>
2016-03-02 01:19:59 +0100

    webserver write the image that receive from app

--- camera/ViewController.swift
+++ camera/ViewController.swift
@@ -14,13 +14,13 @@ import Starscream
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate, WebSocketDelegate {
     let captureSession = AVCaptureSession()
-    let video = AVCaptureVideoDataOutput()
-    let stillImage = AVCaptureStillImageOutput()
+    let video          = AVCaptureVideoDataOutput()
+    let stillImage     = AVCaptureStillImageOutput()
 
     // If we find a device we'll store it here for later use
     var captureDevice : AVCaptureDevice?
     
-    var socket = WebSocket(url: NSURL(string: "http://192.168.1.117:8080")!)
+    var socket = WebSocket(url: NSURL(string: "ws://192.168.0.5:8080")!)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,6 +37,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     
     func websocketDidConnect(socket: WebSocket) {
         print("websocket is connected")
+        print("---")
     }
     
     func websocketDidReceiveData(socket: WebSocket, data: NSData) {
@@ -52,7 +53,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     }
     
     func findDevice(){
-        captureSession.sessionPreset = AVCaptureSessionPresetMedium
+        captureSession.sessionPreset = AVCaptureSessionPreset640x480
         
         let devices = AVCaptureDevice.devices()
         
@@ -86,24 +87,25 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         //let queue:dispatch_queue_t = dispatch_queue_create("mauro-puzza",nil)
         //video.setSampleBufferDelegate(self, queue: queue)
         captureSession.startRunning()
-        NSTimer.scheduledTimerWithTimeInterval(0.2, target: self, selector:  Selector("scatta"), userInfo: nil, repeats: true)
-  
+        // 24 frame
+        // 0 e 1
+        NSTimer.scheduledTimerWithTimeInterval(0.02, target: self, selector:  Selector("scatta"), userInfo: nil, repeats: true)
+        self.socket.writeString("PROVA DA IPHONE VEERSO SERVER"); 
     }
     
     func scatta(){
-        print("CIAO")
         
-        //video.ca
-        
-        //})
+        connessione()
     }
 
 func connessione(){
     let connection:AVCaptureConnection = self.stillImage.connectionWithMediaType(AVMediaTypeVideo)
     self.stillImage.captureStillImageAsynchronouslyFromConnection(connection, completionHandler: {(imageDataSampleBuffer: CMSampleBuffer!, error:  NSError!) in
-        let dataImg:NSData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
-        self.socket.writeData(dataImg)
-
+        if imageDataSampleBuffer != nil{
+            let dataImg:NSData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
+            self.socket.writeData(dataImg)
+        }
+        //self.socket.writeString("Ciao da iphone")
     })
 
 }
@@ -112,20 +114,20 @@ func connessione(){
                         didOutputSampleBuffer sampleBuffer: CMSampleBuffer!,
                         fromConnection connection: AVCaptureConnection!)
     {
-//let jpeg = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
-//socket.writeData(jpeg)
+        //let jpeg = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+        //socket.writeData(jpeg)
         
-            //        let dataImg:NSdata= AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
-//        if let imageBuffer:CVImageBuffer? = CMSampleBufferGetImageBuffer(sampleBuffer){
-//        
-//            // Lock the base address of the pixel buffer
-//            CVPixelBufferLockBaseAddress(imageBuffer!, 0);
-//        
-//            let bytesPerRow:size_t = CVPixelBufferGetBytesPerRow(imageBuffer);
-//            let width:size_t = CVPixelBufferGetWidth(imageBuffer)
-//            let height:size_t = CVPixelBufferGetHeight(imageBuffer)
-//            let src_buff = CVPixelBufferGetBaseAddress(imageBuffer);
-//        }
+        //        let dataImg:NSdata= AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
+        //        if let imageBuffer:CVImageBuffer? = CMSampleBufferGetImageBuffer(sampleBuffer){
+        //
+        //            // Lock the base address of the pixel buffer
+        //            CVPixelBufferLockBaseAddress(imageBuffer!, 0);
+        //
+        //            let bytesPerRow:size_t = CVPixelBufferGetBytesPerRow(imageBuffer);
+        //            let width:size_t = CVPixelBufferGetWidth(imageBuffer)
+        //            let height:size_t = CVPixelBufferGetHeight(imageBuffer)
+        //            let src_buff = CVPixelBufferGetBaseAddress(imageBuffer);
+        //        }
     }
     
     
--- ws-server.rb
+++ ws-server.rb
@@ -3,23 +3,29 @@ require 'faker'
 
 EM.run {
   EM::WebSocket.run(:host => "0.0.0.0", :port => 8080) do |ws|
-    ws.onopen { |handshake|
+    ws.onopen do |handshake|
       puts "WebSocket connection open"
       puts "origin: #{handshake.origin}"
       puts "headers: #{handshake.headers}"
 
       ws.send "Hello Client, you connected to #{handshake.path}"
-    }
+    end
 
     ws.onerror do |error|
       puts "[error] #{error}"
     end
 
-    ws.onclose { puts "Connection closed" }
+    ws.onclose do 
+      puts "Connection closed" 
+    end
 
-    ws.onmessage { |msg|
+    ws.onbinary do |msg|
+      IO.write("/Users/ale/test.jpg", msg)
+    end
+
+    ws.onmessage do |msg|
       puts "message from client: #{msg}"
       ws.send Faker::Hacker.say_something_smart
-    }
+    end
   end
 }
